8dbd6fabe92c89031ae183192043396e45c967a5
mahdi nasirlo <m.nwordpress1223@gmail.com>
2022-07-03 00:25:21 +0430

    install filament and filament form

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,7 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix')
 
-mix.js("resources/js/app.js", "public/js")
-    // .js("./resources/js/home.js", "public/js")
-    // .js("./resources/js/build/ckeditor.js", "public/js")
-    // .postCss("./resources/css/home.css", "public/css")
-    .postCss(
-        "resources/css/app.css",
-        "public/css"
-        // , [require("tailwindcss")]
-    )
-    .version();
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    .version()
